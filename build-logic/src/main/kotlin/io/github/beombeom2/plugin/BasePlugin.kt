604af99d06bd1d39ff3dbd56ffc82b17dfd3f662
BeomBeom2 <whwjdqja20@naver.com>
2025-12-12 23:59:36 +0900

    [Add] compose 플러그인 및 타켓 확장 코드 추가

---
+++ build-logic/src/main/kotlin/io/github/beombeom2/plugin/BasePlugin.kt
@@ -0,0 +1,18 @@
+package io.github.beombeom2.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalog
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.getByType
+
+abstract class BasePlugin : Plugin<Project> {
+    protected val Project.libs: VersionCatalog
+        get() = extensions.getByType<VersionCatalogsExtension>().named("libs")
+
+    protected fun Project.applyPlugin(pluginId: String) {
+        plugins.apply(pluginId)
+    }
+
+    abstract override fun apply(target: Project)
+}
---
+++ build-logic/src/main/kotlin/io/github/beombeom2/plugin/KmpSharedPlugin.kt
@@ -0,0 +1,30 @@
+package io.github.beombeom2.plugin
+
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.getByType
+import org.gradle.kotlin.dsl.invoke
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+
+class KmpSharedPlugin : Plugin<Project> {
+    override fun apply(target: Project): Unit = with(target) {
+        pluginManager.apply("org.jetbrains.kotlin.multiplatform")
+        pluginManager.apply("com.android.library")
+
+        val kotlinExt = extensions.getByType<KotlinMultiplatformExtension>()
+        kotlinExt.apply {
+            androidTarget()
+            iosArm64()
+            iosSimulatorArm64()
+            jvm("desktop")
+
+            sourceSets {
+                val commonMain = getByName("commonMain")
+                commonMain.dependencies {
+                    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+                }
+            }
+        }
+    }
+}
