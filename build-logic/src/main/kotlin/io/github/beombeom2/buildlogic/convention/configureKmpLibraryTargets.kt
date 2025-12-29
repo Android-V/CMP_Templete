b75ba01b3085be9e6b8d2807e1434e7970dd1336
BeomBeom2 <whwjdqja20@naver.com>
2025-12-29 22:45:18 +0900

    [Refactor] kmp 라이브러리 타켓 코드 수정

--- build-logic/src/main/kotlin/io/github/beombeom2/buildlogic/convention/configureKmpLibraryTargets.kt
+++ build-logic/src/main/kotlin/io/github/beombeom2/buildlogic/convention/configureKmpLibraryTargets.kt
@@ -1,49 +1,37 @@
 package io.github.beombeom2.buildlogic.convention
 
-import com.android.build.api.dsl.KotlinMultiplatformAndroidLibraryExtension
 import com.android.build.api.dsl.androidLibrary
+import io.github.beombeom2.buildlogic.Versions
 import io.github.beombeom2.buildlogic.model.AndroidLibraryConfig
+import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinAndroidTarget
-import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 
 fun KotlinMultiplatformExtension.configureLibraryTargets(
     xcfName: String,
     android: AndroidLibraryConfig,
-    androidLibraryAdditional: KotlinMultiplatformAndroidLibraryExtension.() -> Unit = {},
 ) {
     androidLibrary {
-        namespace = android.namespace
         compileSdk = android.compileSdk
         minSdk = android.minSdk
-
-        withHostTestBuilder { }
-
-        withDeviceTestBuilder {
-            sourceSetTreeName = android.deviceTestSourceSetTreeName
-        }.configure {
-            instrumentationRunner = android.instrumentationRunner
-        }
-
-        androidLibraryAdditional()
     }
 
-    targets.withType(KotlinAndroidTarget::class.java).configureEach {
+    targets.withType<KotlinAndroidTarget>().configureEach {
         compilerOptions {
             jvmTarget.set(Versions.JVM_TARGET)
         }
     }
 
-    fun KotlinNativeTarget.configureFramework() {
-        binaries.framework {
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { target ->
+        target.binaries.framework {
             baseName = xcfName
             isStatic = false
         }
     }
-
-    iosX64 { configureFramework() }
-    iosArm64 { configureFramework() }
-    iosSimulatorArm64 { configureFramework() }
 
     jvm("desktop")
 }
